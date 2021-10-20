5871403a9854e4a85dd4dc69b724a54cc0751db4
netbuffer <javawiki@163.com>
2021-10-20 17:54:15 +0800

    test AbstractWebSocketHandler

---
+++ src/main/java/cn/netbuffer/springboot/websocket/demo/websocket/handler/MessageHandler.java
@@ -0,0 +1,42 @@
+package cn.netbuffer.springboot.websocket.demo.websocket.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+@Slf4j
+public class MessageHandler extends AbstractWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        SessionManager.put(session.getId(), session);
+        log.info("session[{}] afterConnectionEstablished", session.getId());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("session[{}] afterConnectionClosed", session.getId());
+        SessionManager.remove(session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("session[{}] receive text message={}", session.getId(), message);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        log.info("session[{}] receive binary message={} length={}", session.getId(), message, message.getPayloadLength());
+    }
+
+    @Override
+    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+        log.info("session[{}] receive pong message={}", session.getId(), message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+    }
+
+}
